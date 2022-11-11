ea5be4d1bb016c2da260899313577af635826b52
daniel021-hub <daes021@hotmail.com>
2022-11-10 23:00:01 -0500

    maquetado1

--- assets/js/data/db.js
+++ assets/js/data/db.js
@@ -3,7 +3,7 @@ export const items = [
     id: 1,
     name: 'Hoodies',
     price: 14.00,
-    image: 'img/featured1.png',
+    image: 'assets/img/featured1.png',
     category: 'hoodies',
     quantity: 10
   },
@@ -11,7 +11,7 @@ export const items = [
     id: 2,
     name: 'Shirts',
     price: 24.00,
-    image: 'img/featured2.png',
+    image: 'assets/img/featured2.png',
     category: 'shirts',
     quantity: 15
   },
@@ -19,7 +19,7 @@ export const items = [
     id: 3,
     name: 'Sweatshirts',
     price: 24.00,
-    image: 'img/featured3.png',
+    image: 'assets/img/featured3.png',
     category: 'sweatshirts',
     quantity: 20
   }
